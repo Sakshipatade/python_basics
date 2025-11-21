00cb4455650b40f65e01c44b4fe6c862b918488e
Sakshipatade <sakshivpatade@gmail.com>
2025-11-21 22:08:32 +0530

    Truth table for operators

---
+++ operators.py
@@ -0,0 +1,17 @@
+# truth table of 'or' operator
+print("True or False is:",True or False)  #output -> True
+print("True or True is:",True or True)    #output -> True
+print("False or True is:",False or True)  #output -> True
+print("False or False is:",False or False)  #output -> False
+
+# truth table of 'and' operator 
+print("True and False is:",True and False)   #output -> False
+print("True and True is:",True and True)     #output -> True
+print("False and True is:",False and True)   #output -> False
+print("False and False is:",False and False)  #output -> False
+
+
+
+# truth table of 'not' operator
+print(not(False)) #output will be True
+print(not(True)) #output will be False
