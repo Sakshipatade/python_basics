ddefe135bb88025ac9c61397d395b24ad455d001
Sakshipatade <sakshivpatade@gmail.com>
2025-11-25 14:32:13 +0530

    String slicing

---
+++ 07_string_positive_slicing.py
@@ -0,0 +1,3 @@
+name = "Sakshi"
+print(name[0:3]) # it will print Sak
+print(name[1:2]) # it will print only a
---
+++ 08_string_negative_slicing.py
@@ -0,0 +1,12 @@
+name = "Sakshi"
+
+print(name[-4:-1]) #it will print ksh 
+print(name[-5:-3]) # it will print ak
+print(name[-6:-1]) #it will print Saksh
+
+#syntax
+'''
+name[num1:num2]
+num1 value is the value we want as the starting 
+num2 value will be excluded but the numbers till the num2 will be print
+'''
