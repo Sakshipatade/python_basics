529be8eb2d7cbafcd2d5def9e3e3398126e76e93
Sakshipatade <sakshivpatade@gmail.com>
2025-11-25 14:37:26 +0530

    string slicing

--- 08_string_negative_slicing.py
+++ 08_string_negative_slicing.py
@@ -10,3 +10,10 @@ name[num1:num2]
 num1 value is the value we want as the starting 
 num2 value will be excluded but the numbers till the num2 will be print
 '''
+
+another_name = "Vaishnavi"
+print(another_name[1:]) #it will print aishnavi
+print(another_name[1:9]) #it will also print aishnavi and 9 is total length of the string
+
+print(another_name[:4]) # it will start from 0 and print till 4 means "Vais"
+print(another_name[0:4]) # it will start from 0 and print till 4 means "Vais"
