2e6279cd7cb7a3799d9b7d7ab5ee372f86457a65
Sakshipatade <sakshivpatade@gmail.com>
2025-11-25 13:49:47 +0530

    Another techinique to sqaured the number added

--- 06_problem.py
+++ 06_problem.py
@@ -1,5 +1,9 @@
 #write a program to calculate the square of the number entered by the user 
 
 num = int(input("Enter the value : "))
-squared_value = num * num
+# squared_value = num * num
+
+# Or
+squared_value = num ** 2
+
 print("The Squared value of num is :", squared_value)
