8d18b0bafa938d951fefdcdd7d009eef56787904
Sakshipatade <sakshivpatade@gmail.com>
2025-11-25 13:47:33 +0530

    To find the squared value entered by the user

--- 06_problem.py
+++ 06_problem.py
@@ -1,1 +1,5 @@
 #write a program to calculate the square of the number entered by the user 
+
+num = int(input("Enter the value : "))
+squared_value = num * num
+print("The Squared value of num is :", squared_value)
