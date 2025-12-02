4cdadce2014427d6e704a86df5753db4e7665938
Sakshipatade <sakshivpatade@gmail.com>
2025-12-02 13:10:25 +0530

    otp generation using user input values

---
+++ delivery_code_generator.py
@@ -0,0 +1,22 @@
+'''
+Ask for city name
+Ask for area name
+Generate a delivery code using slicing:
+First 3 letters of city → uppercase
+Last 2 letters of area → lowercase
+Output format: CITY-xy
+'''
+
+city = input("Enter City: ")
+area = input("Enter Area: ")
+number = input("Enter any 5 number: ") #here numbers are taken as string not 'number' datatype 
+
+uppercase_value_city = city.upper()
+lowercase_value_area = area.lower()
+indexed_city = uppercase_value_city[:3]
+indexed_area = lowercase_value_area[-2:len(lowercase_value_area)]
+indexed_number = number[3:5]
+
+r = indexed_city + indexed_number + indexed_area
+
+print("Generated OTP: ", r)
